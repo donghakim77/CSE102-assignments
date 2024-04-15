38899cd22874576746998aecb4cbdb3a4ffb15eb
donghakim77 <hykdh77@hanyang.ac.kr>
2024-04-16 00:53:49 +0900

    수정

--- pages/index.js
+++ pages/index.js
@@ -10,41 +10,37 @@ export default function Home() {
       </Head>
 
       <main>
+        <div>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Dongha Kim
         </h1>
+        </div>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+         Hanyang Univ. information system
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Youtube &rarr;</h3>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Instagram &rarr;</h3>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>X &rarr;</h3>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Facebook &rarr;</h3>
           </a>
         </div>
       </main>
